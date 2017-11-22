29b03ec5744a76f8ecaa027b144f071a0013afd4
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2017-11-22 11:08:07 +0000

    Add declarations for "pluggable" functions

--- gap/LinAlgPadic.gd
+++ gap/LinAlgPadic.gd
@@ -15,11 +15,15 @@ DeclareGlobalFunction( "MAJORANA_SolutionMatVecs_Padic" );
 # solve for a list of vectors (rationals)
 DeclareGlobalFunction( "MAJORANA_SolutionMatVec_Padic" );
 
-
 DeclareGlobalFunction( "MAJORANA_NullspaceIntMat_Padic" );
 DeclareGlobalFunction( "MAJORANA_NullspaceMat_Padic" );
 
 
 DeclareInfoClass("InfoMajoranaPadics");
 DeclareInfoClass("InfoMajoranaLinearEq");
+
+# This is the function that should be "pluggable" into the
+# rest of the Majorana algebras stuff
+DeclareGlobalFunction( "MAJORANA_SolutionMatVecs_Plugin" );
+DeclareGlobalFunction( "MAJORANA_NullspaceMat_Plugin" );
 
